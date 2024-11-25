870e0dba7743ce73d4297f4f4a0dd1f9f8605631
Evgeny <evgeny.kochenyuk@gmail.com>
2024-11-25 05:07:27 +0100

    CMake

--- Test/TestParser.cpp
+++ Test/TestParser.cpp
@@ -1,10 +1,10 @@
-#include <Lexer.h>
+// #include <Lexer.h>
 
-void test0()
-{
-	Lexer lexer("");
-	for(auto lex:lexer){
-		ASSERTFAIL("unxepected out");
-	}
-	ASSERTSUCCESS();
-}
+// void test0()
+// {
+// 	Lexer lexer("");
+// 	for(auto lex:lexer){
+// 		ASSERTFAIL("unxepected out");
+// 	}
+// 	ASSERTSUCCESS();
+// }
